d334a78da99a2a755f7a0ee150767d4c14b3a83e
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2025-07-16 19:58:31 +0600

    Add chatgpt link

--- generate_dnsmasq_pihole_conf.sh
+++ generate_dnsmasq_pihole_conf.sh
@@ -1,4 +1,5 @@
 # This is a script which will take the list of all the domains and then convert it in such a way so that it can be used with pihole. The main need for this (for me) is I wanted to forward these game domains to a DNS which has caches or lancache serving DNS.
+# CHATGPT: https://chatgpt.com/share/6877afdd-a8b0-8000-b439-0311dbe3413d
 #!/bin/bash
 INPUT="all_domains.txt"
 OUTPUT="/etc/dnsmasq.d/05-lancache-forwarding.conf"
